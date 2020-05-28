3e6d069cbdc43ed92c7e57aecfcad849a93d6c6c
gw-sbirch <60774443+gw-sbirch@users.noreply.github.com>
2020-05-28 10:40:08 +0100

    add garbage collection

--- flavours/nodejs/functionapp/rebuild-v1/index.ts
+++ flavours/nodejs/functionapp/rebuild-v1/index.ts
@@ -1,8 +1,8 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
-import RequestHandlerFactory from "./requestHandlerFactory";
 import { requestHandlerArgs } from "./requestHandler";
+import RequestHandlerFactory from "./requestHandlerFactory";
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest) {
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log("HTTP trigger function processed a request.");
 
     const handler = RequestHandlerFactory.GetRequestHandler(req.url, context);
@@ -22,18 +22,33 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
             });
         }
 
-        if (handlerResponse.rawBody)
-        {
+        context.res.setHeader("Access-Control-Expose-Headers", "*");
+        context.res.setHeader("Access-Control-Allow-Headers", "*");
+        context.res.setHeader("Access-Control-Allow-Origin", "*");
+
+        if (handlerResponse.rawBody) {
             context.res.body = handlerResponse.rawBody;
         }
 
         context.res.statusCode = handlerResponse.statusCode;
-        context.done();
     }
     catch (err) {
         context.log(err);
         context.res.statusCode = 500;
-        context.done();
+    }
+    finally {        
+        try {
+            if (global.gc) {
+                console.log("GC ran");
+                global.gc();
+                context.res.setHeader("GC-RAN", "true");
+            }
+
+            context.res.setHeader("GC-RAN", "false");
+        } catch (e) {
+            console.log("`node --expose-gc index.js`");
+            process.exit();
+        }
     }
 };
 
